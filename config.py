69fffd7cd88dd7648b60976e02b9d3e4b7cd8909
Bsdk0007 <princek8356@gmail.com>
2024-12-24 15:07:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/un-bots/cronus",
+    "https://github.com/Bsdk0007/cronusMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
